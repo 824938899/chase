c655ed7943b0ed8c78f428e09faf486e901db1a9
824938899@qq.com <597148477@qq.com>
2018-09-07 11:16:31 +0800

    first commit

---
+++ src/components/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="home">
+		<!-- header -->
+		<home-header>
+			
+		</home-header>
+		<!-- header -->
+
+		<home-slide>
+			1
+		</home-slide>
+	</div>
+</template>
+
+
+<script>
+	import HomeHeader from 'components/home/pages/HomeHeader.vue'
+	import HomeSlide from 'components/home/pages/HomeSlide.vue'
+
+	export default{
+		name:'Home',
+		components:{
+			'home-header' : HomeHeader,
+			'home-slide' : HomeSlide,
+		}
+	}
+
+</script>
---
+++ src/components/home/pages/HomeHeader.vue
@@ -0,0 +1,22 @@
+<template>
+	<header class="header">
+		this is header
+		
+	</header>
+</template>
+
+
+<script>
+	export default{
+		name:'HomeHeader',
+		
+	}
+</script>
+
+<style lage="stylus" scoped>
+	.header{
+		color:red;
+		height: 50px;
+		font-size: 1rem;
+	}
+</style>
